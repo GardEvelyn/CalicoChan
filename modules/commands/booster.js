19f305bd2a59b2e3ecb6b7691f97a102ff76d6ce
Phong Huynh <cps.phong@gmail.com>
2016-10-06 15:38:48 -0700

    Move booster to edited reply format

--- modules/commands/booster.js
+++ modules/commands/booster.js
@@ -6,25 +6,27 @@ module.exports = function(args){
         console.log(msg.author.username + " booster");
         try{
             let setCode = msg.content.substring(8).trim();
-            request("https://api.magicthegathering.io/v1/sets/" + setCode + "/booster", function(error, response, body) {
-                let bodyJSON = JSON.parse(body);
-                let message = [];
-                if(response.statusCode == 200){
-                    message.push("**" + msg.author + ", your " + setCode.toUpperCase() + " booster pack contained:**");
-                    bodyJSON.cards.forEach(card => {
-                        message.push(card.name);
-                    });
-                }
-                else if(response.statusCode == 404){
-                    for(let i = 0; i < client.strings.booster.unabletofind.length; i++){
-                        message.push(client.strings.booster.unabletofind[i]);
+            msg.reply(`Fetching your ${setCode.toUpperCase()} booster pack. . .`).then(replyToEdit => {
+                request("https://api.magicthegathering.io/v1/sets/" + setCode + "/booster", function(error, response, body) {
+                    let bodyJSON = JSON.parse(body);
+                    let message = [];
+                    if(response.statusCode == 200){
+                        message.push("**" + msg.author + ", your " + setCode.toUpperCase() + " booster pack contained:**");
+                        bodyJSON.cards.forEach(card => {
+                            message.push(card.name);
+                        });
                     }
-                }
-                else{
-                    console.log(response.statusCode);
-                    message.push(client.strings.err);
-                }
-                msg.channel.sendMessage(message);
+                    else if(response.statusCode == 404){
+                        for(let i = 0; i < client.strings.booster.unabletofind.length; i++){
+                            message.push(client.strings.booster.unabletofind[i]);
+                        }
+                    }
+                    else{
+                        console.log(response.statusCode);
+                        message.push(client.strings.err);
+                    }
+                    replyToEdit.edit(message);
+                });
             });
         }
         catch(err){
