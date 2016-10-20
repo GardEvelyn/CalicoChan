24f213a3a4fcdd2a5a1c9d3cd17721513dc86819
Phong Huynh <cps.phong@gmail.com>
2016-10-20 11:40:44 -0700

    Don't bother trying to link to a channel inside a codeblock.

--- modules/commands/tunes.js
+++ modules/commands/tunes.js
@@ -172,7 +172,7 @@ module.exports = function(module_args){
 		return i;
 	}
 
-	module.help = `${client.prefix}tunes <YouTube URL | Query> :: Queues up a tune to play in ${TUNES_VOICE}.\n${client.prefix}tunes skip [index] :: Vote to skip the currently playing tune [or the one at the specified index].\n${client.prefix}tunes queue :: Display the current queue.`;
+	module.help = `${client.prefix}tunes <YouTube URL | Query> :: Queues up a tune to play.\n${client.prefix}tunes skip [index] :: Vote to skip the currently playing tune [or the one at the specified index].\n${client.prefix}tunes queue :: Display the current queue.`;
 
 	return module;
 };
