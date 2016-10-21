7cd5b174139754658a647f15c25eb016a9cf5991
Phong Huynh <cps.phong@gmail.com>
2016-10-21 15:46:31 -0700

    Add markdown to skip message

--- modules/commands/tunes.js
+++ modules/commands/tunes.js
@@ -56,7 +56,7 @@ module.exports = function(module_args){
 			if(TUNES_VOICE.members.get(msg.author.id) != null){
 				song.skipVotes.push(msg.author.id);
 				if(song.requester.id === msg.author.id || song.skipVotes.length >= (Math.ceil((TUNES_VOICE.members.array().length - 1) / 2))){
-					TUNES_CHANNEL.sendMessage(`${song.title} skipped.`).then(() => {
+					TUNES_CHANNEL.sendMessage(`\`${song.title}\` skipped.`).then(() => {
 						msg.delete();
 						recentlySkipped = true;
 						setTimeout(() => {
